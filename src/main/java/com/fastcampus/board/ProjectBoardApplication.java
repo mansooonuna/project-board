82b0083308368a854fe49af7e97065213f7213a0
mansooonuna <parkdasom95@gmail.com>
2023-02-08 21:18:15 +0900

    #13 - AuditingFields 추출
    생성자, 생성일시, 수정자, 수정일시는 
    공통적으로 엔티티 클래스에 들어가며
    도메인과 직접 연관이 없는 요소이므로 추출이 가능함
    
    '@MappedSuperclass' 이용하여 상속 방식으로 추출
    
    ++ "@DateTimeFormat' 요소 추가 및 jpa 옵션 개선

--- src/main/java/com/fastcampus/board/ProjectBoardApplication.java
+++ src/main/java/com/fastcampus/board/ProjectBoardApplication.java
@@ -2,13 +2,13 @@ package com.fastcampus.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class ProjectBoardApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProjectBoardApplication.class, args);
-        System.out.println("hello World");
     }
 
 }
--- src/main/java/com/fastcampus/board/domain/Article.java
+++ src/main/java/com/fastcampus/board/domain/Article.java
@@ -4,13 +4,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.LocalDateTime;
 import java.util.LinkedHashSet;
 import java.util.Objects;
 import java.util.Set;
@@ -23,30 +17,31 @@ import java.util.Set;
         @Index(columnList = "createdAt"),
         @Index(columnList = "createdBy")
 })
-@EntityListeners(AuditingEntityListener.class)
+
 @Entity
-public class Article {
+public class Article extends AuditingFields {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Setter @Column(nullable = false) private String title; //제목
-    @Setter @Column(nullable = false, length = 10000) private String content; //본문
+    @Setter
+    @Column(nullable = false)
+    private String title; //제목
+    @Setter
+    @Column(nullable = false, length = 10000)
+    private String content; //본문
 
-    @Setter private String hashtag; //해시태그
+    @Setter
+    private String hashtag; //해시태그
 
     @ToString.Exclude
     @OrderBy("id")
     @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
     private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
 
-    @CreatedDate @Column(nullable = false) private LocalDateTime createdAt; // 생성일시
-    @CreatedBy @Column(nullable = false, length = 100) private String createdBy; // 생성자
-    @LastModifiedDate @Column(nullable = false) private LocalDateTime modifiedAt; // 수정일시
-    @LastModifiedBy @Column(nullable = false, length = 100) private String modifiedBy; //수정자
 
-
-    protected Article() {}
+    protected Article() {
+    }
 
     private Article(String title, String content, String hashtag) {
         this.title = title;
